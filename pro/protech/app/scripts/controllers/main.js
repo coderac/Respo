1398e756a40ece12764083dc4ef372e153538368
Rahul Mishra <rahul.striker@gmail.com>
2015-01-21 04:29:50 +0530

    Main js file updated

--- pro/protech/app/scripts/controllers/main.js
+++ pro/protech/app/scripts/controllers/main.js
@@ -20,19 +20,31 @@ var ctrls = angular.module('protechApp');
         /*xyz*/
         ];
     });
+/*************************************************************************************************/
+
+/*************************************************************************************************/
+
+var option1Options = ["Java", "UI", "Mobile"];
+var option2Options = [["Core","Servlet","JSP","Spring","struts","Diff. frameworks"],
+  ["HTML","CSS","CANVAS","ANGULARJS","JAVASCRIPT","JQUERY"],
+  ["ANDROID","IOS","BADA","SYMBIAN","ASHA","BBX"]];
+
+ctrls.controller('postProjectCtrl', function($scope){
+/*function categoryCtrl($scope){*/
+
+      $scope.options1 = option1Options;
+      $scope.options2 = []; // we'll get these later
+      $scope.getOptions2 = function(){
+        // just some silly stuff to get the key of what was selected since we are using simple arrays.
+        var key = $scope.options1.indexOf($scope.option1);
+        // Here you could actually go out and fetch the options for a server.
+        var myNewOptions = option2Options[key];
+        // Now set the options.
+        // If you got the results from a server, this would go in the callback
+        $scope.options2 = myNewOptions;
+      };
+  });
 
 
 
 
-/*
-var clientViewCtrl = angular.module('clientViewCtrl', []);
-    clientViewCtrl.controller('clientViewTableCtrl', function($scope){
-    console.log('getting request');
-*/
-/*      $scope.url='../json/clientEmployeeView.json';
-      $http.get($scope.url).success(function(data){}
-        $scope.clientEmployeeView = data;
-      });*//*
-
-    });
-*/
